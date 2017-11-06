1c9ccd157c4e789c57acd0d6c6ed15d652055b84
balagrd2 <32483669+balagrd2@users.noreply.github.com>
2017-11-06 14:51:02 +0530

    Update notification-examples.js

--- notification-examples.js
+++ notification-examples.js
@@ -33,10 +33,19 @@
 
   const allOptionsNotification = function(registration) {
     const title = 'Web Push Book';
-    const options = {
-     body: 'Hello, Welcome to Verticurl',
-      tag: 'vrt',
-      icon: 'images/icons/icon-128x128.png'
+     const options = {
+      body: 'This would be the body text of the notification.\n' +
+        'It can hold two lines of text.',
+      icon: 'images/icons/icon-128x128.png',
+      badge: '/images/demos/badge-128x128.png',
+      image: '/images/demos/unsplash-farzad-nazifi-1600x1100.jpg',
+      tag: 'example-notification',
+      actions: [
+        {
+          action: 'download-book-action',
+          title: 'Download Book',
+          icon: '/images/demos/action-download-book-128x128.png'
+        }
       ]
     };
     registration.showNotification(title, options);
