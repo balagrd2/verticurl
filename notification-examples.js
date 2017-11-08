1532df6b12f99da1c817a1952fdc2a862f5ed1a3
balagrd2 <32483669+balagrd2@users.noreply.github.com>
2017-11-08 14:20:27 +0530

    Update notification-examples.js

--- notification-examples.js
+++ notification-examples.js
@@ -37,7 +37,29 @@
       body: 'Hello, Welcome to Verticurl.\n' +
         'WE THINK GLOBALLY AND DELIVER THE RIGHT RESULTS WHEN YOU NEED THEM.',
       icon: 'images/icons/icon-128x128.png',
-      tag: 'verticurl'
+      tag: 'verticurl',
+        actions: [
+        {
+          action: 'coffee-action',
+          title: 'Coffee',
+          icon: '/images/demos/action-1-128x128.png'
+        },
+        {
+          action: 'doughnut-action',
+          title: 'Doughnut',
+          icon: '/images/demos/action-2-128x128.png'
+        },
+        {
+          action: 'gramophone-action',
+          title: 'gramophone',
+          icon: '/images/demos/action-3-128x128.png'
+        },
+        {
+          action: 'atom-action',
+          title: 'Atom',
+          icon: '/images/demos/action-4-128x128.png'
+        }
+      ]
     };
     registration.showNotification(title, options);
   };
